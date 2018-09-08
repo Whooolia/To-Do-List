34267f478cdc916404113905146ece4729950bfd
[Whooolia] <[tpdms2779@gmail.com]>
2018-09-08 17:21:33 +0100

    to-do-list

--- src/index.js
+++ src/index.js
@@ -1,55 +1,134 @@
 import "./style.css";
 
-// document.querySelector(".doneButton").style.visibility = "hidden";
-// document.querySelector(".deleteButton").style.visibility = "hidden";
 
-// Add list- submit button
+
+// Add list button
 document.querySelector(".add-button").addEventListener("click",function(e){
-	
 	var typed = document.querySelector(".todo-input").value;
 	e.preventDefault();
 	document.querySelector(".todo-input").value = ""; //Something new I leanrt! 
-	// document.querySelector(".doneButton").style.visibility = "visible";
-	// document.querySelector(".deleteButton").style.visibility = "visible";
 
 
 	// keep adding elements
-	var el = document.querySelector(".todo-lists");
+	if(typed.length > 26){
+		alert("Make it short!");
+	} else {
+		var el = document.querySelector(".todo-lists");
 	// 변수이름 역시 항상 의미있는 이름으로 하면 좋음.
-	var newTodoEL = document.createElement("p");
-	newTodoEL.appendChild(document.createTextNode(typed));
-	el.appendChild(newTodoEL);
+		var newDiv = document.createElement("div");
+		var newTodoEL = document.createElement("p");
+		newTodoEL.appendChild(document.createTextNode(typed));
+		el.appendChild(newDiv);
+		newDiv.appendChild(newTodoEL);
 
-	//delete list button
+		//delete list button
 	// 같은 이름의 variable를 재사용할때는 var를 다시 해줄필요가 없음. 그리고 같은 변수 이름으로 주는 것보단 다른 이름으로 주는 게 좋음. 
 	//이에서 이미 el을 선언해줘서 사용할 필요가 없음.
 	//el = document.querySelector(".todo-lists");
 	//위에 newEL을 사용했기 때문에 다른 이름으로 사용하는게 좋음. 
-	var newDoneEL = document.createElement("button");
+	var newDoneEL = document.createElement("button", "i");
+	// newDoneEL = document.createElement("i");
 	// class 이름도 보통 camel Naming(doneButton) 보단 -(dash) 사용하면 좋음.
 	newDoneEL.className = "done-button";
-	newDoneEL.appendChild(document.createTextNode("Done"));
-	el.appendChild(newDoneEL);
+	newDoneEL.className = "fa fa-check";
+	// newDoneEL.appendChild(document.createTextNode("Done"));
+	newDiv.appendChild(newDoneEL);
+
+	
 
 	//el = document.querySelector(".todo-lists");
-	var newDeleteEL = document.createElement("button");
+	var newDeleteEL = document.createElement("button", "i");
 	newDeleteEL.className = "delete-button";
-	newDeleteEL.appendChild(document.createTextNode("Delete"));
-	el.appendChild(newDeleteEL);
+	newDeleteEL.className = "fa fa-trash";
+	// newDeleteEL.appendChild(document.createTextNode("Delete"));
+	newDiv.appendChild(newDeleteEL);
+
+
+
+	newDoneEL.addEventListener("click", function(){
+		// newTodoEL.style.textDecoration = "line-through";
+		newTodoEL.style.color = "white";
+		newTodoEL.style.opacity = "0.7";
+		newDoneEL.style.visibility = "hidden";
+		// newDeleteEL.style.visibility = "hidden";
+	});
+
+	newDeleteEL.addEventListener("click", function(){
+		newTodoEL.remove();
+		newDoneEL.remove();
+		newDeleteEL.remove();
+	});
+	}
+
+	
 });
+
+
 
 document.addEventListener("keypress",function(e) {
 	if(e.keyCode === 13){
 		var typed = document.querySelector(".todo-input").value;
 		e.preventDefault();
 		document.querySelector(".todo-input").value = "";
+
+
+	// keep adding elements
+	if(typed.length > 26){
+		alert("Make it short!");
+	} else {
+		var el = document.querySelector(".todo-lists");
+	// 변수이름 역시 항상 의미있는 이름으로 하면 좋음.
+		var newDiv = document.createElement("div");
+		var newTodoEL = document.createElement("p");
+		newTodoEL.appendChild(document.createTextNode(typed));
+		el.appendChild(newDiv);
+		newDiv.appendChild(newTodoEL);
+
+		//delete list button
+	// 같은 이름의 variable를 재사용할때는 var를 다시 해줄필요가 없음. 그리고 같은 변수 이름으로 주는 것보단 다른 이름으로 주는 게 좋음. 
+	//이에서 이미 el을 선언해줘서 사용할 필요가 없음.
+	//el = document.querySelector(".todo-lists");
+	//위에 newEL을 사용했기 때문에 다른 이름으로 사용하는게 좋음. 
+	var newDoneEL = document.createElement("button", "i");
+	// newDoneEL = document.createElement("i");
+	// class 이름도 보통 camel Naming(doneButton) 보단 -(dash) 사용하면 좋음.
+	newDoneEL.className = "done-button";
+	newDoneEL.className = "fa fa-check";
+	// newDoneEL.appendChild(document.createTextNode("Done"));
+	newDiv.appendChild(newDoneEL);
+
 	
-	var el = document.querySelector(".todo-lists");
-	var newEL = document.createElement("p");
-	newEL.appendChild(document.createTextNode(typed));
-	el.appendChild(newEL);
+
+	//el = document.querySelector(".todo-lists");
+	var newDeleteEL = document.createElement("button", "i");
+	newDeleteEL.className = "delete-button";
+	newDeleteEL.className = "fa fa-trash";
+	// newDeleteEL.appendChild(document.createTextNode("Delete"));
+	newDiv.appendChild(newDeleteEL);
+
+
+
+	newDoneEL.addEventListener("click", function(){
+		// newTodoEL.style.textDecoration = "line-through";
+		newTodoEL.style.color = "white";
+		newTodoEL.style.opacity = "0.7";
+		newDoneEL.style.visibility = "hidden";
+		// newDeleteEL.style.visibility = "hidden";
+	});
+
+	newDeleteEL.addEventListener("click", function(){
+		newTodoEL.remove();
+		newDoneEL.remove();
+		newDeleteEL.remove();
+	});
 	}
+}
+
 });
+
+
+
+
 
 // Clear all
 document.querySelector(".clear-all-button").addEventListener("click",function() {
